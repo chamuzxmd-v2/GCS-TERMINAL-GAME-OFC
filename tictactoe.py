559ac18486f8cba0bf9d17a276877e998031e7e3
chamuzxmd-v2 <chamuzxmd@gmail.com>
2026-02-19 02:38:29 +0530

    Create tictactoe.py

---
+++ tictactoe.py
@@ -0,0 +1,36 @@
+def play_ttt():
+    import os
+    os.system("clear")
+    print("⭕ Tic-Tac-Toe Game ⬛")
+    board = [" "]*9
+
+    def print_board():
+        print(f"{board[0]}|{board[1]}|{board[2]}")
+        print("-+-+-")
+        print(f"{board[3]}|{board[4]}|{board[5]}")
+        print("-+-+-")
+        print(f"{board[6]}|{board[7]}|{board[8]}")
+
+    def check_win(player):
+        win_cond = [(0,1,2),(3,4,5),(6,7,8),(0,3,6),(1,4,7),(2,5,8),(0,4,8),(2,4,6)]
+        for a,b,c in win_cond:
+            if board[a]==board[b]==board[c]==player:
+                return True
+        return False
+
+    player = "X"
+    for _ in range(9):
+        print_board()
+        move = int(input(f"Player {player}, enter position (1-9): "))-1
+        if board[move]==" ":
+            board[move]=player
+            if check_win(player):
+                print_board()
+                print(f"Player {player} wins! 🎉")
+                break
+            player = "O" if player=="X" else "X"
+        else:
+            print("Position occupied!")
+    else:
+        print("It's a draw! 🤝")
+    input("Press Enter to return to menu...")
